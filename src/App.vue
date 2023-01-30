b3d1c5fb3c64ef7dd31aaac86ed9af69074b34ac
shimaxu <shimax15@gmail.com>
2023-01-30 08:11:34 +0500

    setting up vue3,tailwind, eslint and initial scafoloding with routes

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="ml-5 mr-5">
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+      />
+
+      <nav class="space-x-5 bg-gray-100 rounded-lg p-5 mt-5">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/addresses">Addresses</RouterLink>
+        <RouterLink to="/patients">Patients</RouterLink>
+      </nav>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/addresses",
+      name: "addresses",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/addresses/IndexView.vue"),
+    },
+    {
+      path: "/patients",
+      name: "patients",
+      component: () => import("../views/patients/IndexView.vue"),
+    },
+  ],
+});
+
+export default router;
